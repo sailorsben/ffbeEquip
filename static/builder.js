bdc41ea4802e182e62acacbb5c7fd297dbcd0543
Kristoffer Henriksson <swedishkristoffer@gmail.com>
2023-03-06 21:26:05 -0800

    Fix crash when not logged in and actuallyOwnedUnits is unassigned

--- static/builder.js
+++ static/builder.js
@@ -1645,7 +1645,11 @@ function updateUnitLevelDisplay() {
                 $("#unitExAwakeningLevel select").val(builds[currentUnitIndex]._exAwakeningLevel.toString());
             } else {
                 // See if they own the unit. We need to look for both the current unit as well as the braveshift to handle the case where they start with the braveshifted unit.
-                const ownedUnit = actuallyOwnedUnits[builds[currentUnitIndex].unit.id] ?? actuallyOwnedUnits[builds[currentUnitIndex].getBraveshift().unit.id];
+                // If the user is not logged in actuallyOwnedUnits will be undefined so check for that first before using it.
+                let ownedUnit;
+                if (actuallyOwnedUnits) {
+                    ownedUnit = actuallyOwnedUnits[builds[currentUnitIndex].unit.id] ?? actuallyOwnedUnits[builds[currentUnitIndex].getBraveshift().unit.id];
+                }
                 const exRank = $("#autoSelectHighestExLevel").prop('checked') ? ownedUnit?.exRank ?? 1 : 1;
                 $("#unitExAwakeningLevel select").val(exRank.toString());
                 builds[currentUnitIndex].setExAwakeningLevel(exRank);
